3ce3366640a0669637610f136ea3fcaea3390b1c
Arun Kumar M <84022433+Arziva@users.noreply.github.com>
2024-08-19 16:44:11 +0530

    Update app.py
    install rather than path

--- app.py
+++ app.py
@@ -68,13 +68,7 @@ def scrape_places(search_queries, subc):
     results = []
     seen_names = set()
 
-    for search_query in search_queries:
-
-
-        #options = Options()
-        
-        service = Service('/usr/bin/chromedriver')
-
+    for search_query in search_queries: 
         
         options = webdriver.ChromeOptions()
         options.add_argument('--headless')
@@ -82,6 +76,8 @@ def scrape_places(search_queries, subc):
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--disable-gpu')
         options.add_argument('--window-size=1920,1080')
+
+        service = Service(ChromeDriverManager().install())
 
         driver = webdriver.Chrome(service=service, options=options)
 
